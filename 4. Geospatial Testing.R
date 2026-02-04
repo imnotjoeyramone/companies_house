a01a791b28459751d1489d19368eacb4375a32b0
Colin Browne <colinbrowne1987@hotmail.co.uk>
2026-02-04 15:54:26 +0000

    add initial geospatial analysis

---
+++ 4. Geospatial Testing.R
@@ -0,0 +1,27 @@
+######### Initialise ########## 
+
+library(tidyverse)
+library(janitor)
+library(vroom)
+library(sf)
+
+# company pcde list created in 1. Basic Company Data Scoping
+company_pcde_list  <- vroom("data/BasicCompanyData.csv") %>% 
+  select(company_name, company_number, reg_address_post_code, incorporation_date)
+  
+# uprns <- vroom("data/osopenuprn_202601.csv") %>% clean_names()
+
+# ONS postcode database
+ons_pcde <- vroom("data/ONSPD_AUG_2025_UK.csv") %>% clean_names()
+
+get_coords <- 
+  company_pcde_list %>% 
+  inner_join(ons_pcde, by = c("reg_address_post_code" = "postcode_7_char")) %>% 
+  select(company_name, company_number, reg_address_post_code, incorporation_date, latitude, longitude) %>% 
+  filter(incorporation_date >= as.Date("01/01/2025")) %>% 
+  slice_sample(n=1000) %>% 
+  unique() 
+  
+leaflet(get_coords) %>%
+  addTiles() %>%
+  addCircleMarkers(~longitude, ~latitude)
