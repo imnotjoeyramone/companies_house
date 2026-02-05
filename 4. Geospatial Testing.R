46cf78eb85e530f8f8bce882ca305a6f3fe8302f
imnotjoeyramone <30901411+imnotjoeyramone@users.noreply.github.com>
2026-02-05 12:05:33 +0000

    further analysis

--- 4. Geospatial Testing.R
+++ 4. Geospatial Testing.R
@@ -12,6 +12,15 @@ source("functions/dmy_converter.r")
 company_pcde_list  <- vroom("data/BasicCompanyData.csv") %>% 
   clean_names() %>% 
   select(company_name, company_number, reg_address_post_code, incorporation_date)
+
+psc_pcde_list <- read_rds("data/psc_pcde_lkp.rds") 
+
+psc_plus_company_pcde_list <- 
+  company_pcde_list %>% 
+  inner_join(psc_pcde_list, by = "company_number") %>% 
+  mutate(pcde_same = replace_na(ifelse(reg_address_post_code == address_postal_code, TRUE, FALSE), FALSE))
+
+sum(psc_plus_company_pcde_list$pcde_same/nrow(psc_plus_company_pcde_list))
   
 # uprns <- vroom("data/osopenuprn_202601.csv") %>% clean_names()
 
@@ -19,9 +28,11 @@ company_pcde_list  <- vroom("data/BasicCompanyData.csv") %>%
 ons_pcde <- vroom("data/ONSPD_AUG_2025_UK.csv") %>% clean_names()
 
 get_coords <- 
-  company_pcde_list %>% 
-  inner_join(ons_pcde, by = c("reg_address_post_code" = "postcode_7_char")) %>% 
-  select(company_name, company_number, reg_address_post_code, incorporation_date, latitude, longitude) %>% 
+  psc_plus_company_pcde_list %>% 
+  left_join(ons_pcde, by = c("reg_address_post_code" = "postcode_7_char")) %>% 
+  select(company_name, company_number, reg_address_post_code, address_postal_code, incorporation_date, lat_reg_add = latitude, long_reg_add = longitude) %>%
+  left_join(ons_pcde, by = c("address_postal_code" = "postcode_7_char")) %>% 
+  rename(lat_psc_add = latitude, lon_psc_add = longitude) %>% 
   filter(incorporation_date >= as.Date("01/01/2025")) %>% 
   slice_sample(n=1000) %>% 
   unique() 
@@ -29,3 +40,4 @@ get_coords <-
 leaflet(get_coords) %>%
   addTiles() %>%
   addCircleMarkers(~longitude, ~latitude)
+
